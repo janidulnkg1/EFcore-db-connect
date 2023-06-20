23c43bb176cf205fbeaddd4c354eb7d6065536de
janidulnkg1 <janidulnkg1@gmail.com>
2023-06-20 09:19:58 +0530

    add ProductDbContext class

---
+++ efcore-db-mysql/efcore-db-mysql/Controllers/ProductDbContext.cs
@@ -0,0 +1,31 @@
+﻿using efcore_db_mysql.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Storage;
+
+namespace efcore_db_mysql.Controllers
+{
+    public class ProductDbContext : DbContext
+    {
+        public DbSet<Product> Products { get; set; }
+        public ProductDbContext(DbContextOptions<ProductDbContext> dbContextOptions) : base(dbContextOptions)
+        {
+            try
+            {
+                var databaseCreator = Database.GetService<IDatabaseCreator>() as RelationalDatabaseCreator;
+                if(databaseCreator != null) 
+                {
+                    //Creating a Database if it cannot be connected to
+                    if (!databaseCreator.CanConnect()) databaseCreator.Create();
+
+                    //Create Tables if no tables exist
+                    if(!databaseCreator.HasTables()) databaseCreator.CreateTables();
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
